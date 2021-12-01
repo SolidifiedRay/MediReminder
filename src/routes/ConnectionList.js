52a6348de78d6a69f0551199b65a92b86e1c34b7
Xiang Cui <xc1008@nyu.edu>
2021-12-01 09:51:25 -0500

    add calendar user interaction

--- src/routes/ConnectionList.js
+++ src/routes/ConnectionList.js
@@ -1,32 +1,24 @@
 import * as React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import ImageIcon from '@mui/icons-material/Image';
-import WorkIcon from '@mui/icons-material/Work';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import PersonIcon from '@mui/icons-material/Person';
 
 export default function FolderList() {
   return (
     <List style={{ height: 300, overflow: 'scroll' }}>
-      <ListItem style={{ textAlign: 'center', display: 'block' }}>
-        <BeachAccessIcon />
-        <div>connect1</div>
-      </ListItem>
-      <ListItem style={{ textAlign: 'center', display: 'block' }}>
-        <BeachAccessIcon />
-        <div>connect2</div>
-      </ListItem>
-      <ListItem style={{ textAlign: 'center', display: 'block' }}>
-        <BeachAccessIcon />
-        <div>connect3</div>
-      </ListItem>
-      <ListItem style={{ textAlign: 'center', display: 'block' }}>
-        <BeachAccessIcon />
-        <div>connect3</div>
-      </ListItem>
+      {
+        ['Jack', 'Tom', 'Luck'].map((name) => {
+          return <ListItem style={{ textAlign: 'center', display: 'block', cursor: "pointer" }}>
+                  <Avatar style={{ width: '80px', height: '80px', margin: "0 auto" }}>
+                        <PersonIcon
+                          style={{ width: '70px', height: '70px', color: 'black' }}
+                        />
+                    </Avatar>
+                  <div style={{'marginTop': 10, fontWeight: "strong"}}>{name}</div>
+                </ListItem>
+        })
+      }
     </List>
   );
 }
--- src/routes/Modal.js
+++ src/routes/Modal.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -14,6 +14,8 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import Select from '@mui/material/Select';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const style = {
   position: 'absolute',
@@ -37,17 +39,66 @@ const MenuProps = {
   },
 };
 
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri',' Sat']
+
+/*
+
+    {
+      id: uuidv4(),
+      start: new Date('2021-12-01T09:00:00'),
+      end: new Date('2021-12-01T10:00:00'),
+      title: "Take medicine!",
+    },
+    {
+      id: uuidv4(),
+      start: new Date('2021-12-03T09:00:00'),
+      end: new Date('2021-12-03T10:00:00'),
+      title: "Take medicine!"
+    },
+
+*/
+
 export default function BasicModal(props) {
 
   const { open, setOpen} = props
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const [age, setAge] = React.useState('');
+  const [ reminderType, setReminderType ] = React.useState('medication');
+  const [ name, setName ] = useState("")
+  const [ time, setTime ] = useState("12:00am") 
+  const [ weekdays, setWeekdays ] = useState([])
+  const [ dosage, setDosage ] = useState("")
+  const [ extraInfo, setExtraInfo ] = useState("")
+  const { create } = props
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setReminderType(event.target.value);
   };
+
+  const createReminder = (props) => {
+    const reminder = {
+      reminderType,
+      name, time, 
+      weekdays, 
+      dosage, 
+      extraInfo
+    }
+    create([    {
+      id: uuidv4(),
+      start: new Date('2021-12-01T09:00:00'),
+      end: new Date('2021-12-01T10:00:00'),
+      title: "Take medicine!",
+    },
+    {
+      id: uuidv4(),
+      start: new Date('2021-12-03T09:00:00'),
+      end: new Date('2021-12-03T10:00:00'),
+      title: "Take medicine!"
+    },])
+    console.log(reminder)
+    setOpen(false)
+  }
 
   return (
     <div>
@@ -63,60 +114,82 @@ export default function BasicModal(props) {
                     Select Type of Reminder
                     <Select
                         style={{marginTop: 10}}
-                        value={age}
+                        value={reminderType}
                         onChange={handleChange}
                         displayEmpty
                         inputProps={{ 'aria-label': 'Without label' }}
                     >
-                    <MenuItem value="">
-                        <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+                      <MenuItem value="medication">Mediation</MenuItem>
+                      <MenuItem value="reminder">Reminder</MenuItem>
                     </Select>
                 </FormControl>
             </div>
             <div>
-                <TextField id="Medication Name" label="Medication Name" variant="outlined" style={{width: "100%"}}/>
+                <TextField 
+                  id="Medication Name" 
+                  label="Medication Name" 
+                  variant="outlined" 
+                  style={{width: "100%"}}
+                  value={name}
+                  onChange={(e)=>{setName(e.target.value)}}
+
+                />
             </div>
             <div>
-                <input value={"12:00 am"} style={{border: "none", width: "100%", fontSize: 40, textAlign: "center", paddingTop: 30, paddingBottom: 30}}/>
+                <input 
+                  value={time}
+                  onChange={(e)=>{setTime(e.target.value)}}
+                  style={{border: "none", width: "100%", fontSize: 40, textAlign: "center", paddingTop: 30, paddingBottom: 30}}
+                />
             </div>
-            <div style={{display: "flex", justifyContent: "space-around", alignItems: "center", width: "100%", color: "white", textAlign: "center", marginBottom: 20}}>
-                <div style={{background: "#549CDF", borderRadius: "50%", width: 50, height: 50, lineHeight: 3}} >
-                    Sun
-                </div>
-                <div style={{background: "#549CDF", borderRadius: "50%", width: 50, height: 50, lineHeight: 3}} >
-                    Mon
-                </div>
-                <div style={{background: "#549CDF", borderRadius: "50%", width: 50, height: 50, lineHeight: 3}} >
-                    Tue
-                </div>
-                <div style={{background: "#549CDF", borderRadius: "50%", width: 50, height: 50, lineHeight: 3}} >
-                    Wed
-                </div>
-                <div style={{background: "#549CDF", borderRadius: "50%", width: 50, height: 50, lineHeight: 3}} >
-                    Thu
-                </div>
-                <div style={{background: "#549CDF", borderRadius: "50%", width: 50, height: 50, lineHeight: 3}} >
-                    Fri
-                </div>
-                <div style={{background: "#549CDF", borderRadius: "50%", width: 50, height: 50, lineHeight: 3}} >
-                    Sat
-                </div>
+            <div style={{display: "flex", justifyContent: "space-around", alignItems: "center", width: "100%", color: "white", textAlign: "center", marginBottom: 20, cursor: "pointer"}}>
+                {
+                  days.map((day) => {
+                    let color = ''
+                    let textColor = ''
+                    if(weekdays.includes(day)) {
+                      color = "#549CDF"
+                      textColor = 'white'
+                    } else {
+                      color = '#DEDEDE'
+                      textColor = 'black'
+                    }
+                    return  <div 
+                              style={{background: color, borderRadius: "50%", width: 50, height: 50, lineHeight: 3, color: textColor}} 
+                              onClick={() => {
+
+                                if(weekdays.includes(day))  {
+                                  setWeekdays(weekdays.filter((d) => {return d !== day}))
+                                } else {
+                                  setWeekdays([...weekdays, day])
+                                }
+                            }}
+                            >
+                                {day}
+                            </div>
+                  })
+                }
             </div>
             <div style={{marginBottom: 20}}>
-                <TextField id="Dosage" label="Dosage" variant="outlined" style={{width: "100%"}}/>
+                <TextField
+                  value={dosage}
+                  onChange={(e) => {setDosage(e.target.value)}}
+                  id="Dosage" 
+                  label="Dosage" 
+                  variant="outlined" 
+                  style={{width: "100%"}}
+                />
             </div>
             <div>
                 <TextField
-                id="outlined-textarea"
-                label="Multiline Placeholder"
-                placeholder="Placeholder"
-                multiline
-                style={{width: "100%"}}
-                rows={6}
+                  value={extraInfo}
+                  onChange={(e) => {setExtraInfo(e.target.value)}}
+                  id="outlined-textarea"
+                  label="Extra Information"
+                  placeholder="Optional"
+                  multiline
+                  style={{width: "100%"}}
+                  rows={6}
                 />
             </div>
             <div>
@@ -124,7 +197,12 @@ export default function BasicModal(props) {
                     <FormControlLabel control={<Checkbox defaultChecked />} label="Email Reminder" />
                 </FormGroup>
             </div>
-            <Button style={{backgroundColor: "#549CDF", "color": "white", width: "100%"}}> Create </Button>
+            <Button 
+              style={{backgroundColor: "#549CDF", "color": "white", width: "100%"}}
+              onClick={createReminder}
+            > 
+              Create 
+            </Button>
         </Box>
       </Modal>
     </div>
